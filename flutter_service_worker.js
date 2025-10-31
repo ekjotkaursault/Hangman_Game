05b9521472dede980ff8395b2dec1564a3eef7de
ekjotkaursault <ekjotkaur985@gmail.com>
2025-10-31 16:25:13 -0400

    Rebuild for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "97894ce3e8a875d2fd96149d470a562d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c83ef9720793c8d42c862d6a4ff65955",
+"flutter_bootstrap.js": "e4458ce98274888b6a0f89943450b64e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
