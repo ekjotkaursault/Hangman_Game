3f1d26bce984c52d8eec5f6e2da6d341f56f6987
ekjotkaursault <ekjotkaur985@gmail.com>
2025-10-31 11:46:45 -0400

    Added web build for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "97894ce3e8a875d2fd96149d470a562d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a521256f3df68b270d60d7142afa1b40",
+"flutter_bootstrap.js": "021a158de21aea395a5480a2d8b37f4e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
